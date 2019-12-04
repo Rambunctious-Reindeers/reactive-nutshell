23c72227d58c32e731d49a86232f000b84d76a75
Manila Bui <manila.bui@gmail.com>
2019-12-04 11:28:21 -0600

    rearranged event card

--- src/components/event/EventCard.js
+++ src/components/event/EventCard.js
@@ -1,30 +1,28 @@
 import React, { Component } from 'react';
 import "../Nutshell.css";
 
-class EventCard extends Component {
+export default class EventCard extends Component {
     render() {
-
-        const { name, date, location } = this.props.event;
+        const { name, date, location, id } = this.props.event;
+        const { history, deleteEvent, isFirst } = this.props;
 
         const cardContent =
-            <div className="card-content pa3 ma3">
+            <div className="pa3 ma3">
                 <div
                     className="f3 fl pr3 pb4 gray hover-orange pointer" 
                     alt="Edit event"
-                    onClick={() => { this.props.history.push(`/events/${this.props.event.id}/edit`) }}>
+                    onClick={() => { history.push(`/events/${id}/edit`) }}>
                         &#x270E;
                 </div>
                 <p className="f7 fw6">{date}</p>
-                <h3 className="ttu f4 fw6 dib">{name} <span className="dib f5 fw5"> at {location}</span></h3>
-                <button type="button" onClick={() => this.props.deleteEvent(this.props.event.id)}>Delete</button>
-            </div>
+                <h3 className="ttu f4 fw6 dib">{name} <span className="dib f5 fw5"> at </span> {location}</h3>
+                <button type="button" onClick={() => deleteEvent(id)}>Delete</button>
+            </div>;
 
-        const cardContainer = this.props.isFirst ?
-            <div className="card-first pv1">{cardContent}</div> : <div className="card pv1">{cardContent}</div>
-
+        const cardContainer = isFirst 
+            ? <div className="card-first pv1">{cardContent}</div> 
+            : <div className="pv1">{cardContent}</div>;
 
         return cardContainer;
     }
-}
-
-export default EventCard
+};
